bf3dae4fa7cd63922ab267c95ffb802013d3ea74
Team MnM <little.team.mnm@gmail.com>
2012-11-11 16:24:38 -0500

    Initial commit

---
+++ TeamMnMGroupingWebApp/TeamMnMGroupingWebApp/Helper/StudentHelper.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using SlcClient.Entities;
+using SlcClient.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using TeamMnMGroupingWebApp.Models;
+
+namespace TeamMnMGroupingWebApp.Helper
+{
+    public class StudentHelper
+    {
+        public static async Task<StudentDisplayObject> GetStudentDisplayObject(StudentService cs, Student student)
+        {
+            var sections = GetSectionsByStudentId(cs, student.id);
+            var assessments = GetStudentAssessmentsByStudentId(cs, student.id);
+
+            await Task.WhenAll(sections, assessments);
+
+            var result = MapStudentToStudentDisplayObject(student, sections.Result, assessments.Result);
+
+            return result;
+        }
+
+        private static async Task<IEnumerable<Section>> GetSectionsByStudentId(StudentService cs, string studentId)
+        {
+            var result = await cs.GetStudentSectionsByStudentId(studentId);
+            return result;
+        }
+
+        private static async Task<IEnumerable<Assessment>> GetStudentAssessmentsByStudentId(StudentService cs, string studentId)
+        {
+            var result = await cs.GetStudentAssessmentsByStudentId(studentId);
+            return result;            
+        }
+
+        public static StudentDisplayObject MapStudentToStudentDisplayObject(Student student, IEnumerable<Section> sections, IEnumerable<Assessment> assessments)
+        {
+            
+            var newStudent = new StudentDisplayObject();
+            newStudent.id = student.id;
+            newStudent.name = string.Format("{0} {1}", student.name.firstName, student.name.lastSurName);
+            newStudent.sections = from s in sections select s.id;
+            newStudent.disabilities = from d in student.disabilities select FilterHelper.GetEnumDescription(d.disability).title;
+            newStudent.learningStyles = student.learningStyles;
+            newStudent.assessments = assessments;
+
+            return newStudent;
+        }
+
+        private static IEnumerable<SectionDisplayObject> MapSectionToSectionDisplayObject(IEnumerable<Section> sections)
+        {
+            var newSections = new List<SectionDisplayObject>();
+            foreach (var s in sections)
+            {
+                var newSection = new SectionDisplayObject();
+                newSection.courseTitle = s.course.courseTitle;
+                newSection.courseDescription = s.course.courseDescription;
+                newSection.courseLevel = s.course.courseLevel;
+                newSection.subjectArea = s.course.subjectArea;
+                newSections.Add(newSection);
+            }
+            return newSections;
+        }
+    }
+}
---
+++ TeamMnMGroupingWebApp/TeamMnMGroupingWebApp/Models/StudentDisplayObject.cs
@@ -0,0 +1,50 @@
+﻿using SlcClient.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamMnMGroupingWebApp.Models
+{
+    public class StudentDisplayObject
+    {
+        public string id { get; set; }
+
+        /// <summary>
+        /// first name + last name
+        /// </summary>
+        public string name { get; set; }
+
+        /// <summary>
+        /// list of disability names
+        /// </summary>
+        public IEnumerable<string> disabilities { get; set; }
+
+        /// <summary>
+        /// GPA
+        /// </summary>
+        public double cumulativeGradePointAverage { get; set; }
+
+        /// <summary>
+        /// list of sectionId's the students belong to
+        /// </summary>
+        public IEnumerable<string> sections { get; set; }
+
+        public LearningStyles learningStyles { get; set; }
+
+        /// <summary>
+        /// student info
+        /// </summary>
+        //public Student student { get; set; }
+
+        /// <summary>
+        /// sections the student in context is in
+        /// </summary>
+        //public IEnumerable<Section> sections { get; set; }
+
+        /// <summary>
+        /// assessments that the student in context has taken
+        /// </summary>
+        public IEnumerable<Assessment> assessments { get; set; }
+    }
+}
