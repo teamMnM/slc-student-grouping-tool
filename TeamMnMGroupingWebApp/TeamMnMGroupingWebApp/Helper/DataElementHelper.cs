007ef49761c4df52bd35d264b269e2465e9172a2
Team MnM <little.team.mnm@gmail.com>
2012-11-14 21:26:26 -0500

    fix data element text file

---
+++ TeamMnMGroupingWebApp/TeamMnMGroupingWebApp/Helper/DataElementHelper.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using TeamMnMGroupingWebApp.Models;
+
+namespace TeamMnMGroupingWebApp.Helper
+{
+    /// <summary>
+    /// Contruct data elements to display
+    /// </summary>
+    public class DataElementHelper
+    {
+        public static async Task<IEnumerable<DataElement>> InitializeDataElements()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(System.AppDomain.CurrentDomain.BaseDirectory + "\\Data\\DataElements.txt"))
+                {
+                    string s = await sr.ReadToEndAsync();
+                    var list = JsonConvert.DeserializeObject<IEnumerable<DataElement>>(s);
+                    return list;
+                }
+            }
+            catch(Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
