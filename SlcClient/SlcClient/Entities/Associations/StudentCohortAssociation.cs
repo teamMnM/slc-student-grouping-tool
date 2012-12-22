a0a292a36f00226104b761e4df3c342f0605192f
Mike Ng <mikeng13@gmail.com>
2012-12-22 17:38:24 -0500

    fix for not being able to add student to multiple cohorts

--- SlcClient/SlcClient/Entities/Associations/StudentCohortAssociation.cs
+++ SlcClient/SlcClient/Entities/Associations/StudentCohortAssociation.cs
@@ -24,5 +24,23 @@ namespace SlcClient.Entities
         /// The month, day, and year on which the student was removed as part of the cohort.
         /// </summary>
         public DateTime? endDate { get; set; }
+
+        /// <summary>
+        /// Only serialize id if it's not null
+        /// </summary>
+        /// <returns>Whether id should be serialized</returns>
+        public bool ShouldSerializeid()
+        {
+            return (id != null);
+        }
+
+        /// <summary>
+        /// Only serialize endDate if it's not null
+        /// </summary>
+        /// <returns>Whether endDate should be serialized</returns>
+        public bool ShouldSerializeendDate()
+        {
+            return (endDate != null);
+        }
     }
 }
